aed6e1984899ff3f44097483a635ef351136e27d
SeoJungHan <seojh5939@gmail.com>
2023-08-25 17:23:21 +0900

    [FEAT] : ⚙ Notification버튼 구현완료

--- app/src/main/java/bootcamp/sparta/myapplemarket/abstract/BasePageActivity.kt
+++ app/src/main/java/bootcamp/sparta/myapplemarket/abstract/BasePageActivity.kt
@@ -1,10 +1,18 @@
 package bootcamp.sparta.myapplemarket.abstract
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.DialogInterface
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import bootcamp.sparta.myapplemarket.R
 import com.google.android.material.snackbar.Snackbar
 
@@ -19,5 +27,44 @@ abstract class BasePageActivity : AppCompatActivity(){
         val dialog = ExitDialog(positiveButton, "종료", "종료 하시겠습니까?", R.drawable.icon_chat)
         dialog.isCancelable = false
         dialog.show(this.supportFragmentManager, "ExitDialog")
+    }
+
+    fun notification(title: String, content: String) {
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val builder : NotificationCompat.Builder
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "appleMarket-channel"
+            val channelName = "MyAppleMarket"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "사과마켓 알림버튼 클릭시 동작합니다."
+                setShowBadge(true)
+
+                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val audioAttribute = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttribute)
+                enableVibration(true)
+            }
+            manager.createNotificationChannel(channel)
+            builder = NotificationCompat.Builder(this, channelId)
+        } else {
+            builder = NotificationCompat.Builder(this)
+        }
+
+        builder.run {
+            setSmallIcon(R.drawable.notification_bitmap)
+            setWhen(System.currentTimeMillis())
+            setContentTitle(title)
+            setStyle(NotificationCompat.BigTextStyle().bigText(content))
+        }
+
+        manager.notify(1, builder.build())
     }
 }
